5386140b47b26773310812e0fda26fd54ed29561
Ben Morton <ben.morton@kellogg.northwestern.edu>
2024-02-15 03:31:22 +0000

    added taco/cat stuff and companies lnking

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -1,25 +1,38 @@
 class CompaniesController < ApplicationController
 
   def index
+   
+    @companies = Company.all
+
     # find all Company rows
     # render companies/index view
   end
 
-  # def show
-  #   # find a Company
-  #   # render companies/show view with details about Company
-  # end
+  def show
+    # find a Company
+    @company = Company.find_by({"id" => params["id"]})
+    @contacts = Contact.where({"company_id" => @company["id"]})
 
-  # def new
-  #   # render view with new Company form
-  # end
+    # render companies/show view with details about Company
+  end
 
-  # def create
-  #   # start with a new Company
-  #   # assign user-entered form data to Company's columns
-  #   # save Company row
-  #   # redirect user
-  # end
+  def new
+
+    # render view with new Company form
+  end
+
+  def create
+    # start with a new Company
+    @company = Company.new
+    # assign user-entered form data to Company's columns
+    # save Company row
+    @company["name"] = params["name"]
+    @company["city"] = params["city"]
+    @company["state"] = params["state"]
+    @company.save
+    # redirect user
+    redirect_to "/companies"
+  end
 
   # def edit
   #   # find a Company
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -11,12 +11,30 @@ class PostsController < ApplicationController
     # render posts/new view with new Post form
   end
 
-  # def create
-  #   # start with a new Post
-  #   @post = Post.new
-  #   # assign user-entered form data to Post's columns
-  #   # save Post row
-  #   # redirect user
-  # end
+  def create
+    # start with a new Post
+    @post = Post.new
+    # assign user-entered form data to Post's columns
+    @post["author"] = params["author"]
+    @post["body"] = params["body"]
+    @post["image"] = params["image"]
+    @post.save
+    # save Post row
+   redirect_to "/posts"
+  end
 
+end
+
+
+def create
+  # start with a new Company
+  @company = Company.new
+  # assign user-entered form data to Company's columns
+  # save Company row
+  @company["name"] = params["name"]
+  @company["city"] = params["city"]
+  @company["state"] = params["state"]
+  @company.save
+  # redirect user
+  redirect_to "/companies"
 end
